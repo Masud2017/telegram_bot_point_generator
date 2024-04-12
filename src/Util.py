680cefa213e9a9c9ee7f4bc7a671c1038566239e
Masud2017 <msmasud578@gmail.com>
2024-04-12 15:00:04 +0600

    Fixed get_admin_user id issue for regular admin

--- src/Util.py
+++ src/Util.py
@@ -1,16 +1,22 @@
 from random import SystemRandom
 from telegram import Update
 from telegram.ext import ContextTypes
+from telegram import constants
 import re
 
 async def get_admin_user_id(update:Update,context:ContextTypes) -> None:
     data = await context.bot.getChatAdministrators(update.message.chat.id)
+    current_user_id = update.message.from_user["id"]
+
+    # print("printing the chat data  ",data)
     admin_user_id = 0
     for x in data:
-        if x.status == "creator":
-            admin_user_id = x.user.id
-            break
-
+        
+        if x.status == constants.ChatMemberStatus.OWNER or x.status == constants.ChatMemberStatus.ADMINISTRATOR:
+            if x.user.id == current_user_id:
+                admin_user_id = x.user.id
+                break
+        
     return admin_user_id
 
 
