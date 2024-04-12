a63e2b7ae619f7b9cb9ea2e8dbe76f4aa807d02c
Masud2017 <msmasud578@gmail.com>
2024-04-12 18:35:10 +0600

    added logging settijng

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 import os
 from src.Application import Application
+from logging import basicConfig, getLogger, INFO
+
+basicConfig(level=INFO)
+log = getLogger()
 
 load_dotenv()
 # print(os.environ["TELEGRAM_TOKEN"])
