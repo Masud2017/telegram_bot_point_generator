dd1a364ab346f0281c0e82852e1a2870ef248afd
Masud2017 <msmasud578@gmail.com>
2024-04-13 11:08:35 +0600

    Fixed the endless reply from bot after additem

--- src/CommandCollection.py
+++ src/CommandCollection.py
@@ -210,7 +210,7 @@ class CommandCollection:
 
         session = session_handler.get_user_session_obj(str(current_user_id))
         if session != None:
-            if session.phase > 1: # for fixing endless reply from bot
+            if session["phase"] > 1: # for fixing endless reply from bot
                 session_handler.remove_user_session(str(current_user_id))   
 
         db.init_users(str(current_user_id),update.message) # this function will only work if the user is new and does not have any record else  it will be ignored
